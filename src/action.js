474eb77d6124fd5313405f06215cc9c1c2f9a3cb
jvbsl <jvsbl@gmx.de>
2021-09-17 00:47:20 +0200

    Actually build

--- src/action.js
+++ src/action.js
@@ -73,13 +73,9 @@ const parseCommandLine = async () => {
     }
     const searchDir = core.getInput('search_path', { required: false });
     if (searchDir !== "") {
-        params.push("-s");
-        params.push(searchDir);
     }
     const destinationDir = core.getInput('destination_dir', { required: false });
     if (destinationDir !== "") {
-        params.push("-d");
-        params.push(destinationDir);
     }
     return params;
 };
