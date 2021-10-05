1529739ac0f73816ed9f29119bd246b88394e751
jvbsl <jvsbl@gmx.de>
2021-10-05 13:19:56 +0200

    Updated to newest litgit version

--- src/litgit_version.js
+++ src/litgit_version.js
@@ -1,4 +1,4 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.litgit_version = void 0;
-exports.litgit_version = '0.2.0.44-alpha';
+exports.litgit_version = '0.2.0.47-alpha';
--- src/litgit_version.ts
+++ src/litgit_version.ts
@@ -1,1 +1,1 @@
-export const litgit_version : string = '0.2.0.44-alpha';
+export const litgit_version : string = '0.2.0.47-alpha';
