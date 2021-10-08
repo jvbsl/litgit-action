d7d57fe81dcd7ed72030f3791731e3d7011609aa
jvbsl <jvsbl@gmx.de>
2021-10-08 14:35:56 +0200

    Updated to newest litgit

--- dist/index.js
+++ dist/index.js
@@ -2670,7 +2670,7 @@ run();
 
 Object.defineProperty(exports, "__esModule", ({ value: true }));
 exports.litgit_version = void 0;
-exports.litgit_version = '0.2.0.47-alpha';
+exports.litgit_version = '0.2.0.48-alpha';
 
 
 /***/ }),
--- src/litgit_version.ts
+++ src/litgit_version.ts
@@ -1,1 +1,1 @@
-export const litgit_version : string = '0.2.0.47-alpha';
+export const litgit_version : string = '0.2.0.48-alpha';
